38f7248328b610dbbfef4e70ca5110dcdeb6f25b
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2023-12-12 13:48:20 +0100

    Add isSelected function

---
+++ src/Functions/IsSelected.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FormsComputedLanguage\Functions;
+
+use FormsComputedLanguage\Exceptions\ArgumentCountException;
+use FormsComputedLanguage\Exceptions\TypeException;
+
+/**
+ * Run the isSelected() function, a wrapper over in_array.
+ * Call: isSelected(array $array, string $option)
+ */
+class IsSelected
+{
+    public const FUNCTION_NAME = 'isSelected';
+
+    /**
+     * Run the isSelected() function.
+     *
+     * @param array $args Array of arguments.
+     * @return bool Indicates whether the option is in the array of selected items.
+     */
+    public static function run($args)
+    {
+        $argc = (int)(count($args));
+        if ($argc !== 2) {
+            throw new ArgumentCountException("the isSelected() function called with {$argc} arguments, but has exactly two required arguments");
+        }
+        if (!is_array($args[0])) {
+            $type = gettype($args[0]);
+            throw new TypeException("isSelected() called with {$type} as first argument, requires an array");
+        }
+        return in_array($args[1], $args[0], true);
+    }
+}
