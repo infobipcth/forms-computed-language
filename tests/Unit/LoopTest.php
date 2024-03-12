4b3a1f7a1401f774a5cf726a6f715ca99cb21904
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2024-03-12 17:24:33 +0100

    Add tests for foreaching

---
+++ tests/Unit/LoopTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use FormsComputedLanguage\LanguageRunner;
+
+test('foreaching an array works', function () {
+    $lr = new LanguageRunner;
+    $code = <<<'CODE'
+    $a = [3, 1, 4];
+    foreach ($a as $index => $value) {
+        $a[$index] = $value + 1;
+    }
+    CODE;
+    $lr->setCode("$code");
+    $lr->setVars([]);
+    $lr->evaluate();
+    expect($lr->getVars())->toBe(['a' => [4, 2, 5]]);
+});
+
