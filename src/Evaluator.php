8fa5165d5fe7e28a48104ae3e534040e7809c717
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2024-03-12 17:24:06 +0100

    Support array dimension fetching, assignment and iteration

--- src/Evaluator.php
+++ src/Evaluator.php
@@ -18,6 +18,7 @@ use FormsComputedLanguage\StackObjects\ArrayItem as StackObjectsArrayItem;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\AssignOp;
@@ -52,6 +53,7 @@ use PhpParser\Node\Scalar;
 use PhpParser\Node\Stmt\Else_;
 use PhpParser\Node\Stmt\ElseIf_;
 use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Foreach_;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
@@ -192,8 +194,6 @@ class Evaluator extends NodeVisitorAbstract
             }
         }
 
-
-
         // Start evaluating nodes.
 
         if ($node instanceof Scalar) { // If this node is a scalar, push its value to the stack.
@@ -201,7 +201,12 @@ class Evaluator extends NodeVisitorAbstract
         }
 
         if ($node instanceof Variable) { // If this node references a variable e.g. $x, push the variable value to the stack.
-            $this->stack[] = $this->vars[$node->name] ?? null;
+            if (!($node->getAttribute('parentIsAssignment', false))) {
+                $this->stack[] = $this->vars[$node->name] ?? null;
+            }
+            else {
+                $this->stack[] = $node->name ?? null;
+            }
         }
 
         // Set up relationships and references for children of If, Elseif, Else blocks and ternary operators.
@@ -261,6 +266,36 @@ class Evaluator extends NodeVisitorAbstract
             $node->else->setAttribute('parentTernary', $node);
             $node->else->setAttribute('parentTernaryRelationship', 'else');
         }
+
+        if ($node instanceof Assign) {
+            $node->var->setAttribute('parentIsAssignment', true);
+            $node->var->setAttribute('parentAssign', $node);
+        }
+
+        if ($node instanceof ArrayDimFetch) {
+            $node->var->setAttribute('parentIsAssignment', $node->getAttribute('parentIsAssignment', false));
+            //$node->dim->setAttribute('parentIsAssignment', $node->getAttribute('parentIsAssignment', false));
+        }
+
+        if ($node instanceof Foreach_) {
+            $iteratedArray = $this->vars[$node->expr->name] ?? [];
+            foreach ($iteratedArray as $iterationKey => $iterationValue) {
+                $isolatedLoopContextTraverser = new NodeTraverser();
+                $mockedLr = new LanguageRunner; // todo: configuration inheritance
+                $iterationVars = [
+                    ...$this->vars, 
+                    $node->keyVar->name => $iterationKey,
+                    $node->valueVar->name => $iterationValue
+                ];
+                $isolatedLoopContextEvaluator = new Evaluator($iterationVars, $mockedLr);
+                $isolatedLoopContextTraverser->addVisitor($isolatedLoopContextEvaluator);
+                $isolatedLoopContextTraverser->traverse($node->stmts);
+                $afterIterationVars = $mockedLr->getVars();
+                $this->vars = $afterIterationVars;
+            }
+            unset($this->vars[$node->keyVar->name], $this->vars[$node->valueVar->name]);
+            return NodeTraverser::DONT_TRAVERSE_CHILDREN;
+        }
     }
 
     /**
@@ -281,7 +316,29 @@ class Evaluator extends NodeVisitorAbstract
         $nodeType = get_class($node);
 
         if ($node instanceof Assign) {
-            $this->vars[$node->var->name] = array_pop($this->stack);
+            if ($node->getAttribute('isArrayAssignment', false)) {
+                $dimensional = $node->getAttribute('isArrayAssignmentByDim', false);
+                $value = array_pop($this->stack);
+                if ($dimensional) {
+                    $dim = array_pop($this->stack);
+                }
+                $name = array_pop($this->stack);
+                if (!$dimensional) {
+                    $this->vars[$name][] = $value;
+                }
+                else {
+                    $this->vars[$name][$dim] = $value;
+                }
+            } else {
+                if (!($node->dim ?? false)) {
+                    $this->vars[$node->var->name] = array_pop($this->stack);
+                }
+                else {
+                    $arrayDim = array_pop($this->stack);
+                    $arrayVal = array_pop($this->stack);
+                    $this->vars[$node->var->name][$arrayDim] = $arrayVal;
+                }
+            }
         } elseif ($node instanceof AssignOp) {
             if ($node instanceof AssignOpPlus) {
                 $this->vars[$node->var->name] += array_pop($this->stack);
@@ -372,7 +429,7 @@ class Evaluator extends NodeVisitorAbstract
             if ($node?->key) {
                 $arrayItemKey = array_pop($this->stack);
             }
-            $arrayItem = new StackObjectsArrayItem($arrayItemKey, $arrayItemValue);
+            $arrayItem = new StackObjectsArrayItem($arrayItemKey ?? null, $arrayItemValue);
             $this->stack[] = $arrayItem;
         } elseif ($node instanceof Array_) {
             $arraySize = count($node->items);
@@ -386,6 +443,24 @@ class Evaluator extends NodeVisitorAbstract
                 }
             }
             $this->stack[] = array_reverse($array);
+        } elseif ($node instanceof ArrayDimFetch) {
+            if (!($node->getAttribute('parentIsAssignment', false))) {
+                $arrayDim = array_pop($this->stack);
+                $array = array_pop($this->stack);
+                $this->stack[] = $array[$arrayDim];
+            }
+            else {
+                $assignmentNode = $node->getAttribute('parentAssign');
+                $assignmentNode->setAttribute('isArrayAssignment', true);
+                if ($node->dim ?? false) {
+                    $assignmentNode->setAttribute('isArrayAssignmentByDim', true);
+                    $arrayDim = array_pop($this->stack);
+                    $this->stack[] = $arrayDim;
+                } 
+                $arrayName = array_pop($this->stack);
+                $this->stack[] = $arrayName;
+                
+            }
         } elseif (
             $node instanceof Variable
             || $node instanceof Scalar
@@ -396,6 +471,7 @@ class Evaluator extends NodeVisitorAbstract
             || $node instanceof Name
             || $node instanceof Arg
             || $node instanceof Expression
+            || $node instanceof Foreach_
         ) {
             // Don't throw an UnknownTokenException for nodes we consider in enterNode or that are 'wrapper' nodes,
             // such as Expression, Arg, Name etc.
