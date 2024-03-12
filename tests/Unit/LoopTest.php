01de8b00a27bed960ad4536cd469c2924abe32eb
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2024-03-12 17:35:54 +0100

    Add an additional test for foreaching without index

--- tests/Unit/LoopTest.php
+++ tests/Unit/LoopTest.php
@@ -16,3 +16,16 @@ test('foreaching an array works', function () {
     expect($lr->getVars())->toBe(['a' => [4, 2, 5]]);
 });
 
+test('foreaching an array works when index is ommited', function () {
+    $lr = new LanguageRunner;
+    $code = <<<'CODE'
+    $a = [3, 1, 4];
+    foreach ($a as $value) {
+        $a[] = $value + 1;
+    }
+    CODE;
+    $lr->setCode("$code");
+    $lr->setVars([]);
+    $lr->evaluate();
+    expect($lr->getVars())->toBe(['a' => [3, 1, 4, 4, 2, 5]]);
+});
