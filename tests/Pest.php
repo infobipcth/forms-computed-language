026a1fbba0ae60fc775af27e8af0b55ad0e77dd7
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2024-04-03 14:36:22 +0200

    Improve tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,4 +5,6 @@ use PHPUnit\Framework\TestCase;
 
 uses(TestCase::class)->beforeEach(function() {
     $this->languageRunner = LanguageRunner::getInstance();
+    $this->languageRunner->setConstantBehaviour('whitelist');
+    $this->languageRunner->setAllowedConstants(['true', 'false']);
 })->group('unit')->in('Unit');
