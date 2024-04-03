9d8719a6fcbb10489983311c253fd9f10a8a10a2
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2024-04-03 14:31:37 +0200

    Add execution change exceptions to handle return values

---
+++ src/Visitors/ExecutionChangeExceptions/ExecutionChangeException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FormsComputedLanguage\Visitors\ExecutionChangeExceptions;
+
+class ExecutionChangeException extends \Exception
+{
+    public static int $change;
+    public function getChange() : ?int {
+        return static::$change ?? null;
+    }
+
+    public static function throw() {
+        $exception = new (static::class);
+        //$exception->change = $_change != -1234 ? $_change : static::$change;
+        throw $exception;
+    }
+}
