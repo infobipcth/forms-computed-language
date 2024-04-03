690226fe0c94ef7d6344153f1b97090044ac347b
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2024-04-03 14:34:53 +0200

    Add a new stack

---
+++ src/Lifecycle/Stack.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FormsComputedLanguage\Lifecycle;
+
+use FormsComputedLanguage\Helpers;
+
+class Stack
+{
+    private static $stack = [];
+    public static function push($value): void
+    {
+            static::$stack[] = $value;
+    }
+
+    public static function pop() {
+        return array_pop(static::$stack);
+    }
+
+    public static function peek() {
+        return Helpers::arrayEnd(static::$stack);
+    }
+}
