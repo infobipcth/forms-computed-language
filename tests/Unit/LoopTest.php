045b2f574ce8fb94f57acd8dd74f88ef1558d15c
Denis Žoljom <dingo-d@users.noreply.github.com>
2024-04-30 11:09:47 +0200

    Tests for keyed arrays looping (#8)
    * Add tests for foreach
    
    * Add space for more visibility in foreach visitor
    
    * Add tests for exposing variables inside the loop
    
    Break test works, continue not, not sure why, as simpler ones are working fine.
    
    * Fix the failing test

--- tests/Unit/LoopTest.php
+++ tests/Unit/LoopTest.php
@@ -44,3 +44,34 @@ test('check that foreach with control flow works', function(){
     $this->languageRunner->evaluate();
     expect($this->languageRunner->getVars())->toBe(['a' => [0, 1, 4, 5, 6]]);
 });
+
+test('check that foreach with control flow works and array keys', function(){
+    $code = <<<'CODE'
+    $a = ['first' => 1, 'second' => 2];
+    foreach ($a as $index => $value) {
+        if ($index == 'first') {
+            $a[$index] = $value + 3;
+        }
+    }
+    CODE;
+    $this->languageRunner->setCode("$code");
+    $this->languageRunner->setVars([]);
+    $this->languageRunner->evaluate();
+    expect($this->languageRunner->getVars())->toBe(['a' => ['first' => 4, 'second' => 2]]);
+});
+
+test('check that foreach with assignment inside the loop works', function(){
+    $code = <<<'CODE'
+    $a = ['first' => 1, 'second' => 2];
+
+    foreach ($a as $index => $value) {
+        if ($index == 'first') {
+            $b[] = $value + 3;
+        }
+    }
+    CODE;
+    $this->languageRunner->setCode("$code");
+    $this->languageRunner->setVars([]);
+    $this->languageRunner->evaluate();
+    expect($this->languageRunner->getVars())->toBe(['a' => ['first' => 1, 'second' => 2], 'b' => [4]]);
+});
